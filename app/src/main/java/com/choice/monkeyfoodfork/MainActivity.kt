5b59dc278e93d44b54d0d5fbd283773aad1f02bd
choicedev <fabricio_zamora1@outlook.com>
2023-01-30 23:41:43 -0300

    test work #11

--- app/src/main/java/com/choice/monkeyfoodfork/MainActivity.kt
+++ app/src/main/java/com/choice/monkeyfoodfork/MainActivity.kt
@@ -10,7 +10,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity(){//simsera
+class MainActivity : ComponentActivity(){//simseras
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
